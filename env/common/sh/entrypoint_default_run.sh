57fb64bd21e2941c7176d04b0ec07d888ce5cc47
liujinsuo <710419489@qq.com>
2020-09-28 06:56:29 +0800

    add py

--- env/common/sh/entrypoint_default_run.sh
+++ env/common/sh/entrypoint_default_run.sh
@@ -5,8 +5,11 @@ sh $DOCKERENV/import.sh entrypoint
 # 吸收 cmd 的指令与参数
 if [ $# -eq 0 ]; then
     # 如果未添加 cmd ，则保持容器的运行
+    echo "------------cmd is null-----------------"
     tail -f /dev/null
 else 
     # 如果添加了cmd，则执行cmd
+    echo "------------run cmd-----------------"
+
     exec $@
 fi
---
+++ hdspark/pyspark/entrypoint/main.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+
+echo "--------------pyspark-------------------"
+jupyter notebook --allow-root
