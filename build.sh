0de23a74205d013019e55b0818e97f70bac36efa
liujinsuo <710419489@qq.com>
2020-09-27 22:22:56 +0800

    add cluster sh

--- build.sh
+++ build.sh
@@ -5,7 +5,3 @@ docker build -t hdnode ./env/hdnode
 docker build -t hd-hive-base ./hive/base
 docker build -t hd-hive-spark ./hdspark
 docker build -t hdnginx ./hdnginx
-# docker-compose -f hdcluster/docker-compose_hd.yml up -d
-# docker-compose -f hdcluster/docker-compose_hd.yml ps
-# docker-compose -f hdcluster/docker-compose_hd.yml down
-# docker-compose -f hdcluster/docker-compose_hd_zk.yml up -d
---
+++ cluster.sh
@@ -0,0 +1,4 @@
+docker-compose -f hdcluster/docker-compose_hd.yml up $@
+# docker-compose -f hdcluster/docker-compose_hd.yml up -d
+# docker-compose -f hdcluster/docker-compose_hd.yml ps
+# docker-compose -f hdcluster/docker-compose_hd.yml down
