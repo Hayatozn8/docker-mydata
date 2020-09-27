79fda36d6dc2df0f6b3d8901709c5c64da7e227d
liujinsuo <710419489@qq.com>
2020-09-28 02:20:07 +0800

    add py

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ docker build -t env-ssh ./env/ssh
 docker build -t env-ssh-jdk8 ./env/jdk8/
 docker build -t hdnode ./env/hdnode
 docker build -t hd-hive-base ./hive/base
-docker build -t hd-hive-spark ./hdspark
+docker build -t hd-hive-spark ./hdspark/hd-hive-spark
 docker build -t hdnginx ./hdnginx
