405fca12b580af1b059a4df966c725f61d5c5e85
liujinsuo <710419489@qq.com>
2020-11-17 21:00:41 +0800

    change image's naming rule

--- bin/build.sh
+++ bin/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 IMG_PATH="$(cd `dirname $0`;pwd)/../img"
-docker build -t env-common      $IMG_PATH/env/common
-docker build -t env-jdk8        $IMG_PATH/env/jdk8
-docker build -t env-ssh         $IMG_PATH/env/ssh
-docker build -t env-zknode      $IMG_PATH/env/zknode
-docker build -t env-hanode      $IMG_PATH/env/hanode
+docker build -t env-common      $IMG_PATH/env-common
+docker build -t env-jdk8        $IMG_PATH/env-jdk8
+docker build -t env-ssh         $IMG_PATH/env-ssh
+docker build -t env-zknode      $IMG_PATH/env-zknode
+docker build -t env-hanode      $IMG_PATH/env-hanode
 # docker build -t hive-base       $IMG_PATH/hive/base
 # docker build -t hd-hive-spark   $IMG_PATH/hdspark/hd-hive-spark
 #docker build -t hd-hive-pyspark $IMG_PATH/hdspark/pyspark
--- bin/kafka.sh
+++ bin/kafka.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 IMG_PATH="$(cd `dirname $0`;pwd)/../img"
-docker build -t kafka-base          $IMG_PATH/kafka/base
-docker build -t kafka-eagle-node    $IMG_PATH/kafka/eagle/node
-docker build -t kafka-eagle-webui   $IMG_PATH/kafka/eagle/webui
+docker build -t kafka-base          $IMG_PATH/kafka-base
+docker build -t kafka-eagle-node    $IMG_PATH/kafka-eagle-node
+docker build -t kafka-eagle-webui   $IMG_PATH/kafka-eagle-webui
 # docker build -t hive-base       $IMG_PATH/hive/base
 # docker build -t hd-hive-spark   $IMG_PATH/hdspark/hd-hive-spark
 #docker build -t hd-hive-pyspark $IMG_PATH/hdspark/pyspark
