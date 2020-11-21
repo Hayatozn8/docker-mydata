0cc8d1b858cddc7699108c4cca50d064caed2661
liujinsuo <710419489@qq.com>
2020-11-21 10:11:40 +0800

    update .gitignore & add resource/

--- sbin/hdcluster.sh
+++
@@ -1,5 +0,0 @@
-CONF_PATH="$(cd `dirname $0`;pwd)/../conf"
-docker-compose -f $CONF_PATH/hdcluster/docker-compose_hd.yml $@
-# docker-compose -f hdcluster/docker-compose_hd.yml up -d
-# docker-compose -f hdcluster/docker-compose_hd.yml ps
-# docker-compose -f hdcluster/docker-compose_hd.yml down
