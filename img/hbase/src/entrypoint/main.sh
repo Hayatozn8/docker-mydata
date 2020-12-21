25516b0803f56b0cc72c06d9f4edd0e2c6398458
liujinsuo <710419489@qq.com>
2020-12-21 21:10:22 +0800

    update hbase

--- img/hbase/src/entrypoint/main.sh
+++ img/hbase/src/entrypoint/main.sh
@@ -21,7 +21,48 @@ do
     echo "$regionserver" >> $HBASE_HOME/conf/regionservers
 done
 
+# 创建 conf/backup-masters
+if [ ! -z "$HB_BACKUP_MASTERS" ];then
+    touch "$HBASE_HOME/conf/backup-masters"
+    for master in ${HB_BACKUP_MASTERS//,/ };do
+        echo ${master} >> "$HBASE_HOME/conf/backup-masters"
+    done
+fi
+
+# $1 ip
+# $2 hostname
+# function tryRegisterHost()
+# {
+#     # 如果从未写入过 /etc/hosts，则写入
+#     if [ -z  $(grep "$1\s+$2$" /etc/hosts) ]; then
+#         echo "$1 $2" >> /etc/hosts
+#     fi
+# }
+
+# 尝试创建与某个 IP/hostname 下的 user 的 ssh 连接
+# $1 user
+# $2 IP/hostname
+function tryCreateSSHConnect()
+{
+    sshTarget="$1@$2"
+    # 尝试进行 ssh 连接，并且失败时不重试
+    # 失败时会输出异常 Host key verification failed.
+    ssh -o NumberOfPasswordPrompts=0 $sshTarget "pwd"
+    # 如果无法通过 ssh 进行连接，则创建连接
+    if [ $? != 0 ]; then
+        echo "can't connect $sshTarget, try to create connection"
+        createSSHConnect.sh $sshTarget
+    fi
+}
+
 # 启动HBase集群
 if [ "$HB_MASTER" = 'true' ];then
+    # 与高可用集群的其他master结点建立ssh连接
+    if [ ! -z "$HB_BACKUP_MASTERS" ];then
+        for master in ${HB_BACKUP_MASTERS//,/ };do
+            tryCreateSSHConnect "root" $master
+        done
+    fi
+
     start-hbase.sh
 fi
